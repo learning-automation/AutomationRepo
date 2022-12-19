5eb0eb048354ece0bce9bcf0c174a1e33cb2b8e4
sayaligalande <j22.sayali@gmail.com>
2022-12-19 21:06:57 +0530

    Changes in Products File

--- src/test/java/testcases/ui/products/TC_Products.java
+++ src/test/java/testcases/ui/products/TC_Products.java
@@ -27,33 +27,95 @@ public class TC_Products {
         br.launchBrowser();
         br.maximize();
         driver = br.getDriver();
+        br.navigateUrl(browser.getProperty("url"));
         products = new Products((br.getDriver()));
+        products.clickProducts();
+        products.hoverProducts1();
+    }
+
+  /*  @Test
+    public void tc001_parabank_Products() {
+        br.navigateUrl(browser.getProperty("url"));
+        products.clickProducts(); // Products on Home Page
+
     }
 
     @Test
-    public void parabank_Products_001(){
+    public void tc002_parabank_Products() {
         br.navigateUrl(browser.getProperty("url"));
+        products.hoverProducts1(); // Products on Parasoft Page
 
-        products.clickProducts();
+    }*/
+
+    @Test
+    public void tc003_parabank_Products() {
         products.hoverProducts1();
-        products.clickParasoft_C();
-        products.hoverProducts1();
-        products.clickParasoft_JTest();
-        products.hoverProducts1();
-        products.clickParasoft_dotTest();
-        products.hoverProducts1();
-        products.clickParasoft_Insure();
-        products.hoverProducts1();
-        products.clickParasoft_DTP();
-        products.hoverProducts1();
-        products.clickParasoft_CTP();
-        products.hoverProducts1();
-        products.clickParasoft_Selenic();
-        products.hoverProducts1();
-        products.clickParasoft_Soatest();
-        products.hoverProducts1();
-        products.clickParasoft_virtualize();
-        products.hoverProducts1();
+        products.clickParasoft_C(); // Parasoft C/C++
 
     }
+
+    @Test
+    public void tc004_parabank_Products() {
+        products.hoverProducts1();
+        products.clickParasoft_JTest();
+
+    }
+
+    @Test
+    public void tc005_parabank_Products() {
+        products.hoverProducts1();
+        products.clickParasoft_JTest();
+
+    }
+
+    @Test
+    public void tc006_parabank_Products() {
+        products.hoverProducts1();
+        products.clickParasoft_dotTest();
+
+    }
+
+    @Test
+    public void tc007_parabank_Products() {
+        products.hoverProducts1();
+        products.clickParasoft_Insure();
+
+    }
+
+    @Test
+    public void tc008_parabank_Products() {
+        products.hoverProducts1();
+        products.clickParasoft_DTP();
+
+    }
+
+    @Test
+    public void tc009_parabank_Products() {
+        products.hoverProducts1();
+        products.clickParasoft_CTP();
+
+    }
+
+    @Test
+    public void tc010_parabank_Products() {
+        products.hoverProducts1();
+        products.clickParasoft_Selenic();
+
+    }
+
+    @Test
+    public void tc011_parabank_Products() {
+        products.hoverProducts1();
+        products.clickParasoft_Soatest();
+
+    }
+
+    @Test
+    public void tc012_parabank_Products() {
+        products.hoverProducts1();
+        products.clickParasoft_virtualize();
+
+    }
+
+
 }
