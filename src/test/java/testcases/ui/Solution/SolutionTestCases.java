a64f240623cdb9ffa439f917f4cc18708f7146a5
SachinMahabare <aug22.sachinm@gmail.com>
2022-12-21 22:59:50 +0530

    Sachin changes files

--- src/test/java/testcases/ui/Solution/SolutionTestCases.java
+++ src/test/java/testcases/ui/Solution/SolutionTestCases.java
@@ -21,7 +21,7 @@ public class SolutionTestCases {
     WebDriver driver ;
     Properties browser ;
     Properties testCase;
-    SolutionPage solupage;
+    SolutionPage solution;
 
     @BeforeSuite
     public void readBrowser(){
@@ -32,209 +32,64 @@ public class SolutionTestCases {
 
 
     @BeforeMethod
-    public void readdata(){
+    public void readData(){
         br.launchBrowser();
         br.maximize();
         driver = br.getDriver();
         login = new LoginPage(br.getDriver());
-        solupage = new SolutionPage(br.getDriver());
+        solution = new SolutionPage(br.getDriver());
         testCase = TestDataReader.readProperties("TC_001.properties");
         br.navigateUrl(browser.getProperty("url"));
         login.enterUsername(testCase.getProperty("username"));
         login.enterPassword(testCase.getProperty("password"));
         login.clickLogIn();
-        solupage.clickOnProduct();
+        solution.clickOnProduct();
 
     }
-
     @Test
     public void TC_001_solution() {
-        Actions action = new Actions(driver);
-        WebElement SolutionMain = driver.findElement(By.linkText("Solutions"));
-        action.moveToElement(SolutionMain).perform();
-        WebElement subApisecurity = driver.findElement(By.linkText("API Security Testing"));
-        action.moveToElement(subApisecurity).perform();
-        action.click().build().perform();
+        br.navigateUrl(browser.getProperty("url"));
+        solution.clickOnSolution();
+        solution.clickApiSecurityTesting();
+        solution.clickOnSolution();
+        solution.clickAPITesting();
+        solution.clickOnSolution();
+        solution.clickApplSecTest();
+        solution.clickOnSolution();
+        solution.clickAutomatedTesting();
+        solution.clickOnSolution();
+        solution.clickCodeCoverage();
+        solution.clickOnSolution();
+        solution.clickCodeQuality();
+        solution.clickOnSolution();
+        solution.clickContinuousTesting();
+        solution.clickOnSolution();
+        solution.clickFunctinalSafetyCompliance();
+        solution.clickOnSolution();
+        solution.clickLowCodeApplicationTesting();
+        solution.clickOnSolution();
+        solution.clickPerformanceTesting();
+        solution.clickOnSolution();
+        solution.clickReportingAnalytics();
+        solution.clickOnSolution();
+        solution.clickRequirementsTracebility();
+        solution.clickOnSolution();
+        solution.clickServiceVirtulization();
+        solution.clickOnSolution();
+        solution.clickSmartTestExecution();
+        solution.clickOnSolution();
+        solution.clickStaticCodeAnalysis();
+        solution.clickOnSolution();
+        solution.clickTestDataManagement();
+        solution.clickOnSolution();
+        solution.clickTestEnvironmentProvisioning();
+        solution.clickOnSolution();
+        solution.clickUnitTesting();
+        solution.clickOnSolution();
+        solution.clickWebUITesting();
+        solution.clickOnSolution();
+
     }
 
-    @Test
-    public void TC_002_solution(){
-        Actions action = new Actions(driver);
-        WebElement SolutionMain = driver.findElement(By.linkText("Solutions"));
-        action.moveToElement(SolutionMain).perform();
-        WebElement subApisecurity = driver.findElement(By.linkText("API Testing"));
-        action.moveToElement(subApisecurity).perform();
-        action.click();
-    }
-
-    @Test
-    public void TC_003_solution(){
-        Actions action = new Actions(driver);
-        WebElement SolutionMain = driver.findElement(By.linkText("Solutions"));
-        action.moveToElement(SolutionMain).perform();
-        WebElement subApisecurity = driver.findElement(By.linkText("Application Security Testing"));
-        action.moveToElement(subApisecurity).perform();
-        action.click();
-    }
-
-    @Test
-    public void TC_004_solution(){
-        Actions action = new Actions(driver);
-        WebElement SolutionMain = driver.findElement(By.linkText("Solutions"));
-        action.moveToElement(SolutionMain).perform();
-        WebElement subApisecurity = driver.findElement(By.linkText("Automated Testing"));
-        action.moveToElement(subApisecurity).perform();
-        action.click();
-    }
-
-    @Test
-    public void TC_005_solution(){
-        Actions action = new Actions(driver);
-        WebElement SolutionMain = driver.findElement(By.linkText("Solutions"));
-        action.moveToElement(SolutionMain).perform();
-        WebElement subApisecurity = driver.findElement(By.linkText("Code Coverage"));
-        action.moveToElement(subApisecurity).perform();
-        action.click().build().perform();
-    }
-
-    @Test
-    public void TC_006_solution(){
-        Actions action = new Actions(driver);
-        WebElement SolutionMain = driver.findElement(By.linkText("Solutions"));
-        action.moveToElement(SolutionMain).perform();
-        WebElement subApisecurity = driver.findElement(By.linkText("Code Quality"));
-        action.moveToElement(subApisecurity).perform();
-        action.click();
-    }
-
-    @Test
-    public void TC_007_solution(){
-        Actions action = new Actions(driver);
-        WebElement SolutionMain = driver.findElement(By.linkText("Solutions"));
-        action.moveToElement(SolutionMain).perform();
-        WebElement subApisecurity = driver.findElement(By.linkText("Continuous Testing"));
-        action.moveToElement(subApisecurity).perform();
-        action.click();
-    }
-
-    @Test
-    public void TC_008_solution(){
-        Actions action = new Actions(driver);
-        WebElement SolutionMain = driver.findElement(By.linkText("Solutions"));
-        action.moveToElement(SolutionMain).perform();
-        WebElement subApisecurity = driver.findElement(By.linkText("Functional Safety Compliance"));
-        action.moveToElement(subApisecurity).perform();
-        action.click();
-    }
-
-    @Test
-    public void TC_009_solution(){
-        Actions action = new Actions(driver);
-        WebElement SolutionMain = driver.findElement(By.linkText("Solutions"));
-        action.moveToElement(SolutionMain).perform();
-        WebElement subApisecurity = driver.findElement(By.linkText("Low-Code Application Testing"));
-        action.moveToElement(subApisecurity).perform();
-        action.click();
-    }
-
-    @Test
-    public void TC_010_solution(){
-        Actions action = new Actions(driver);
-        WebElement SolutionMain = driver.findElement(By.linkText("Solutions"));
-        action.moveToElement(SolutionMain).perform();
-        WebElement subApisecurity = driver.findElement(By.linkText("Performance Testing"));
-        action.moveToElement(subApisecurity).perform();
-        action.click();
-    }
-
-    @Test
-    public void TC_011_solution(){
-        Actions action = new Actions(driver);
-        WebElement SolutionMain = driver.findElement(By.linkText("Solutions"));
-        action.moveToElement(SolutionMain).perform();
-        WebElement subApisecurity = driver.findElement(By.linkText("Reporting & Analytics"));
-        action.moveToElement(subApisecurity).perform();
-        action.click();
-    }
-
-    @Test
-    public void TC_012_solution(){
-        Actions action = new Actions(driver);
-        WebElement SolutionMain = driver.findElement(By.linkText("Solutions"));
-        action.moveToElement(SolutionMain).perform();
-        WebElement subApisecurity = driver.findElement(By.linkText("Requirements Traceability"));
-        action.moveToElement(subApisecurity).perform();
-        action.click();
-    }
-
-    @Test
-    public void TC_013_solution(){
-        Actions action = new Actions(driver);
-        WebElement SolutionMain = driver.findElement(By.linkText("Solutions"));
-        action.moveToElement(SolutionMain).perform();
-        WebElement subApisecurity = driver.findElement(By.linkText("Service Virtualization"));
-        action.moveToElement(subApisecurity).perform();
-        action.click();
-    }
-
-    @Test
-    public void TC_014_solution(){
-        Actions action = new Actions(driver);
-        WebElement SolutionMain = driver.findElement(By.linkText("Solutions"));
-        action.moveToElement(SolutionMain).perform();
-        WebElement subApisecurity = driver.findElement(By.linkText("Smart Test Execution"));
-        action.moveToElement(subApisecurity).perform();
-        action.click();
-    }
-
-    @Test
-    public void TC_015_solution(){
-        Actions action = new Actions(driver);
-        WebElement SolutionMain = driver.findElement(By.linkText("Solutions"));
-        action.moveToElement(SolutionMain).perform();
-        WebElement subApisecurity = driver.findElement(By.linkText("Static Code Analysis"));
-        action.moveToElement(subApisecurity).perform();
-        action.click();
-    }
-
-    @Test
-    public void TC_016_solution(){
-        Actions action = new Actions(driver);
-        WebElement SolutionMain = driver.findElement(By.linkText("Solutions"));
-        action.moveToElement(SolutionMain).perform();
-        WebElement subApisecurity = driver.findElement(By.linkText("Test Data Management"));
-        action.moveToElement(subApisecurity).perform();
-        action.click();
-    }
-
-    @Test
-    public void TC_017_solution(){
-        Actions action = new Actions(driver);
-        WebElement SolutionMain = driver.findElement(By.linkText("Solutions"));
-        action.moveToElement(SolutionMain).perform();
-        WebElement subApisecurity = driver.findElement(By.linkText("Test Environment Provisioning"));
-        action.moveToElement(subApisecurity).perform();
-        action.click();
-    }
-
-    @Test
-    public void TC_018_solution(){
-        Actions action = new Actions(driver);
-        WebElement SolutionMain = driver.findElement(By.linkText("Solutions"));
-        action.moveToElement(SolutionMain).perform();
-        WebElement subApisecurity = driver.findElement(By.linkText("Unit Testing"));
-        action.moveToElement(subApisecurity).perform();
-        action.click();
-    }
-
-    @Test
-    public void TC_019_solution(){
-        Actions action = new Actions(driver);
-        WebElement SolutionMain = driver.findElement(By.linkText("Solutions"));
-        action.moveToElement(SolutionMain).perform();
-        WebElement subApisecurity = driver.findElement(By.linkText("Web UI Testing"));
-        action.moveToElement(subApisecurity).perform();
-        action.click();
-    }
 
 }
