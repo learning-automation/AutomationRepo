66759bb2ff370e0e6d3dc10a7e622b47a557ceae
Pankaj B <pankajbhambhani01@gmail.com>
2022-12-22 09:19:44 +0530

    changes due to merge - issues

--- src/test/java/testcases/ui/login/LoginInValidCases.java
+++ src/test/java/testcases/ui/login/LoginInValidCases.java
@@ -42,6 +42,15 @@ public class LoginInValidCases {
         Assert.assertEquals(loginPage.getErrorElement().getText(), testCase.getProperty("errorMsg"));
     }
 
+    @Test
+    public void tc001_parabank_loginwithoutPassword(){
+        testCase = TestDataReader.readProperties("tc001.properties");
+        br.navigateUrl(browser.getProperty("url"));
+        loginPage.enterUsername(testCase.getProperty("username"));
+        loginPage.clickLogIn();
+        Assert.assertEquals(loginPage.getErrorElement().getText(), testCase.getProperty("errorMsg"));
+    }
+
     // @Test - only enter username -> click login -> validate error
     // @Test - only enter password -> click login -> validate error
     // @Test -> blank -> click login -> validate error
