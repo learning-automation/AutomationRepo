d85b19e3aa60fc17e6390dd3fad90a61c200eabd
Ashwini Sonawane <aug22.ashwinisonawane@gmail.com>
2023-01-03 07:44:39 +0530

    new changes added

--- src/main/java/practice/ashwini/Program15.java
+++ src/main/java/practice/ashwini/Program15.java
@@ -5,4 +5,20 @@ package practice.ashwini;
 
 public class Program15 {
 
-}
+
+
+        public static void main(String[] args) {
+            String name = "my name is pankaj";
+            String temp = "";
+            String[] splitedString = name.split("");
+            for (int i = 0; i < splitedString.length; i++) {
+                if (i % 2 == 0) {
+                    temp = temp + "" + splitedString[i].toLowerCase();
+                } else {
+                    temp = temp + "" + splitedString[i].toUpperCase();
+                }
+            }
+            System.out.println("String is : " + temp);
+        }
+    }
+
