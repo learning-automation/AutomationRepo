1c43deb4ae17c720b2f8dd9df2af4ed5f4d8d849
PankajPredator <aug22.pankaj@gmail.com>
2023-01-05 06:35:46 +0530

    Hi new changes are

--- src/main/java/practice/pankaj/CountOccurance.java
+++ src/main/java/practice/pankaj/CountOccurance.java
@@ -4,32 +4,28 @@ import java.util.Scanner;
 public class CountOccurance {
 
 
+    public static void main(String args[]) {
+        String strn;
+        int i, len;
+        int counter[] = new int[256];
+        Scanner scan = new Scanner(System.in);
+        System.out.print("Please enter a string: ");
+        strn = scan.nextLine();
 
-        public static void main(String args[])
-        {
-            String strn;
-            int i, len;
-            int counter[] = new int[256];
-            Scanner scan = new Scanner(System.in);
-            System.out.print("Please enter a string: ");
-            strn = scan.nextLine();
+        len = strn.length();
 
-            len = strn.length();
+        for (i = 0; i < len; i++) {
+            counter[(int) strn.charAt(i)]++;
+        }
 
-            for (i = 0; i < len; i++)
-            {
-                counter[(int) strn.charAt(i)]++;
-            }
+        for (i = 0; i < 256; i++) {
+            if (counter[i] != 0) {
 
-            for (i = 0; i < 256; i++)
-            {
-                if (counter[i] != 0)
-                {
-
-                    System.out.println((char) i + " --> " + counter[i]);
-                }
+                System.out.println((char) i + " --> " + counter[i]);
             }
         }
+    }
+}
 
 
 
