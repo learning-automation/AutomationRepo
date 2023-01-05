b9aa20e8d9c8d985f1b26e0dd47471bc88871503
PankajPredator <aug22.pankaj@gmail.com>
2023-01-05 18:26:23 +0530

    New edit for string

---
+++ src/main/java/practice/pankaj/StringManipulate.java
@@ -0,0 +1,18 @@
+package practice.pankaj;
+import java.util.*;
+public class StringManipulate {
+
+
+        public static void main(String[] args) {
+            Scanner sc = new Scanner(System.in);
+            System.out.println("Enter a string :");
+            String str = sc.nextLine();
+            str = str.toLowerCase();
+            char[] ch = str.toCharArray();
+            for(int i=0; i<ch.length; i=i+2){
+                ch[i] = Character.toUpperCase(ch[i]);
+            }
+            System.out.println(new String(ch));
+        }
+    }
+
